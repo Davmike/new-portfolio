87f3214025fa348d0691d127aa50c63e779aaa6b
David Mikeladze <davidmikeladze00@gmail.com>
2024-06-13 18:44:20 +0400

    feat: add about page texts

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -3,7 +3,26 @@ import React from "react";
 function About() {
   return (
     <div className="bg-[red]">
-      <h1>ABOUT</h1>
+      {/* about section div */}
+      <div>
+        <h1>ABOUT</h1>
+        <div></div>
+        <p>mikeladzedav@gmail.com</p>
+      </div>
+      {/* about info text */}
+      <p>
+        Front-End Developer with 1+ years of experience in creating responsive
+        web applications using JavaScript and TypeScript. Skilled in React,
+        Vue.js, HTML5, CSS3, and npm. Proficient in component-based development
+        and RxJS. Experienced in mentoring and leading projects to successful
+        completion.
+      </p>
+      {/* personal info */}
+      <div>
+        <div></div>
+        <p>Tbilis, Georgia</p>
+        <div></div>
+      </div>
     </div>
   );
 }
