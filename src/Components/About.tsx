361638d25649eb9fb5fb57bf8f43f0ccb0d9e421
David Mikeladze <davidmikeladze00@gmail.com>
2024-06-13 19:03:46 +0400

    feat: add about styles

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,28 +1,32 @@
-import React from "react";
+import test from "../assets/test.png";
 
 function About() {
   return (
-    <div className="bg-[red]">
-      {/* about section div */}
+    <div className="text-[white] px-[20px] py-[20px]">
+      {/* second main div */}
       <div>
-        <h1>ABOUT</h1>
-        <div></div>
-        <p>mikeladzedav@gmail.com</p>
+        {/* about section div */}
+        <div>
+          <h1 className="font-bold text-[20px]">ABOUT</h1>
+          <div className="w-[66px] h-[1px] bg-[#175b4b]"></div>
+          <p className="text-[12px]">mikeladzedav@gmail.com</p>
+        </div>
+        {/* about info text */}
+        <p className="text-[17px] mt-[50px]">
+          Front-End Developer with 1+ years of experience in creating responsive
+          web applications using JavaScript and TypeScript. Skilled in React,
+          Vue.js, HTML5, CSS3, and npm. Proficient in component-based
+          development and RxJS. Experienced in mentoring and leading projects to
+          successful completion.
+        </p>
+        {/* personal info */}
+        <div>
+          <div></div>
+          <p>Tbilis, Georgia</p>
+          <div></div>
+        </div>
       </div>
-      {/* about info text */}
-      <p>
-        Front-End Developer with 1+ years of experience in creating responsive
-        web applications using JavaScript and TypeScript. Skilled in React,
-        Vue.js, HTML5, CSS3, and npm. Proficient in component-based development
-        and RxJS. Experienced in mentoring and leading projects to successful
-        completion.
-      </p>
-      {/* personal info */}
-      <div>
-        <div></div>
-        <p>Tbilis, Georgia</p>
-        <div></div>
-      </div>
+      <img src={test} alt="" />
     </div>
   );
 }
