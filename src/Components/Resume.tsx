31e7cc1f827eceb38919fc158e7b3ca9a5a42765
David Mikeladze <davidmikeladze00@gmail.com>
2024-06-30 15:25:05 +0400

    feat: add personal skilss

--- src/Components/Resume.tsx
+++ src/Components/Resume.tsx
@@ -9,11 +9,15 @@ function Resume() {
     { title: "Node.js", src: node, value: "90" },
   ];
   const languages = [
-    { title: "English" },
-    { title: "Germany" },
-    { title: "Georgian" },
+    { title: "English", value: "50" },
+    { title: "Germany", value: "60" },
+    { title: "Georgian", value: "70" },
   ];
-  const personalSkills = ["Communication", "Teamwork", "Problem Solving"];
+  const personalSkills = [
+    { title: "Communication", dot: "▫️" },
+    { title: "Teamwork", dot: "▫️" },
+    { title: "Problem Solving" },
+  ];
   const experiences = [
     "Software Developer at XYZ Corp (2018-2021)",
     "Frontend Developer at ABC Inc (2021-Present)",
@@ -72,28 +76,48 @@ function Resume() {
         </div>
 
         {/* LANGUAGE container */}
-        <div className="relative text-white">
+        <div className="relative text-white mt-[20px] ">
           <h3 className="font-bold">LANGUAGE</h3>
           <div className="w-[66px] h-[1px] bg-[#175b4b]"></div>
           {languages.map((item, index) => (
-            <p key={index}>{item.title}</p>
+            <div
+              key={index}
+              className="mt-[10px] flex justify-center items-center gap-[10px]"
+            >
+              <div className="flex w-[170px] gap-[10px]">
+                <p>{item.title}</p>
+              </div>
+              <input
+                type="range"
+                min="1"
+                max="100"
+                value={item.value}
+                className="slider"
+                disabled
+              />
+            </div>
           ))}
         </div>
 
         {/* PERSONAL SKILLS */}
-        <div className="relative text-white">
+        <div className="relative text-white mt-[20px] ">
           <h3 className="font-bold">PERSONAL SKILLS</h3>
           <div className="w-[66px] h-[1px] bg-[#175b4b]"></div>
-          {personalSkills.map((item, index) => (
-            <p key={index}>{item}</p>
-          ))}
+          <div className="flex items-center flex-row gap-[10px]">
+            {personalSkills.map((item, index) => (
+              <div className="flex mt-[10px]">
+                <p key={index}>{item.title}</p>
+                <div className="ml-[10px]">{item.dot}</div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
 
       {/* main div for experience and education */}
       <div>
         {/* EXPERIENCE */}
-        <div className="relative text-white">
+        <div className="relative text-white mt-[20px]">
           <h3 className="font-bold">EXPERIENCE</h3>
           <div className="w-[66px] h-[1px] bg-[#175b4b]"></div>
           {experiences.map((item, index) => (
@@ -114,7 +138,7 @@ function Resume() {
       {/* main div for what can i do, interest and hobby */}
       <div>
         {/* WHAT CAN I DO */}
-        <div className="relative text-white">
+        <div className="relative text-white mt-[20px]">
           <h3 className="font-bold">WHAT CAN I DO</h3>
           <div className="w-[66px] h-[1px] bg-[#175b4b]"></div>
           {whatCanIDo.map((item, index) => (
@@ -123,7 +147,7 @@ function Resume() {
         </div>
 
         {/* INTEREST */}
-        <div className="relative text-white">
+        <div className="relative text-white mt-[20px]">
           <h3 className="font-bold">INTEREST</h3>
           <div className="w-[66px] h-[1px] bg-[#175b4b]"></div>
           {interests.map((item, index) => (
@@ -132,7 +156,7 @@ function Resume() {
         </div>
 
         {/* HOBBY */}
-        <div className="relative text-white">
+        <div className="relative text-white mt-[20px]">
           <h3 className="font-bold">HOBBY</h3>
           <div className="w-[66px] h-[1px] bg-[#175b4b]"></div>
           {hobbies.map((item, index) => (
