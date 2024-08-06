eae598a9f47e0d43ecd79630eec94c299857c368
David Mikeladze <davidmikeladze00@gmail.com>
2024-08-06 20:04:32 +0400

    feat: add scroll effect with gsap on footer section

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,6 +1,24 @@
+import { useEffect } from "react";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+import ScrollReveal from "scrollreveal";
 
 function Footer() {
+  // on scroll show component smooth effect
+  useEffect(() => {
+    const config = {
+      origin: "top",
+      distance: "50px",
+      duration: 1000,
+      delay: 300,
+      opacity: 0,
+      easing: "ease-in-out",
+      reset: true,
+      viewFactor: 0.5,
+    };
+
+    ScrollReveal().reveal("#reset", config);
+  }, []);
+
   return (
     <>
       <div className="h-[1px] w-[100%] bg-[white] mt-[50px]"></div>
@@ -8,27 +26,25 @@ function Footer() {
         <div className="max-w-4xl mx-auto">
           <div className="flex flex-col md:flex-row justify-between items-start md:items-center">
             <div className="mb-4 md:mb-0">
-              <h2 className="text-2xl font-bold">David Mikeladze</h2>
-              <p className="mt-2">
+              <h2 className="text-2xl font-bold" id="reset">
+                David Mikeladze
+              </h2>
+              <p className="mt-2" id="reset">
                 Experienced web developer who{" "}
                 <span className="text-[#F04D40] font-bold">enjoys</span>{" "}
-                building <span className="text-[#F04D40] font-bold">fast</span>{" "}
-                and <span className="text-[#F04D40] font-bold">accessible</span>{" "}
+                building{" "}
+                <span className="text-[#F04D40] font-bold" id="reset">
+                  fast
+                </span>{" "}
+                and{" "}
+                <span className="text-[#F04D40] font-bold" id="reset">
+                  accessible
+                </span>{" "}
                 digital products.
               </p>
             </div>
             <div className="flex flex-col md:flex-row md:space-x-8">
-              {/* <div className="mb-4 md:mb-0">
-                <h3 className="text-xl font-bold">Contact</h3>
-                <p className="mt-2">
-                  Gmail:{" "}
-                  <a href="" className="text-[#F04D40] font-bold">
-                    {" "}
-                    mikeladzedav@gmail.com
-                  </a>
-                </p>
-              </div> */}
-              <div>
+              <div id="reset">
                 <h3 className="text-xl font-bold">Social</h3>
                 <div className="flex space-x-4 mt-2">
                   <a
@@ -56,7 +72,10 @@ function Footer() {
               </div>
             </div>
           </div>
-          <div className="flex justify-center items-center mt-[20px]">
+          <div
+            className="flex justify-center items-center mt-[20px]"
+            id="reset"
+          >
             <p className="text-[15px] font-normal">2024 @ David Mikeladze</p>
           </div>
         </div>
