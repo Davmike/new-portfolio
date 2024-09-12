d23aa86aee5e8084a55bd738c7ab58a193a958f1
Sick_Mac! <david@Davids-MacBook-Pro.local>
2024-09-12 22:28:25 +0400

    feat: add dark mode on home page

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -3,9 +3,13 @@ import { FaFacebook, FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
 import { BiLogoGmail } from "react-icons/bi";
 import { FaXTwitter } from "react-icons/fa6";
 import ScrollReveal from "scrollreveal";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { MyContext } from "./Context";
 
 function Home() {
+  const context = useContext(MyContext);
+  const { darkMode }: any = context;
+
   // on scroll show hello text smooth effect
   useEffect(() => {
     const config = {
